5ee6f733f7d3f036eef234c637a974c2ba798f55
Stelian <stelimania@gmail.com>
2021-08-09 15:15:47 +0300

    added gamemaster

--- RockPaperScissors.sol
+++ RockPaperScissors.sol
@@ -19,22 +19,22 @@ contract RockPaperScissors{
     }
 
     address public gamemaster;
+    address winner;
+    
     mapping(address => Player) public players;
-    Player[] gamers;
+    address[] gamers;
     uint moves;
     
     // Allowed withdrawals in case of Draw
     mapping(address => uint) pendingReturns;
     
-    
-    modifier condition(bool _condition) {
-        require(_condition);
-        _;
-    }
-    
     event GameStarted(uint gameid);
     event Win(address winner, address losser, uint amount, string message);
-    event Draw();
+    event Draw(string message);
+    
+    constructor(){
+        gamemaster = msg.sender;
+    }
     
     function registerToPlay(address player2)  public payable {
        
@@ -52,9 +52,12 @@ contract RockPaperScissors{
             msg.value == 1 ether,
             "Bet must be 1 ether"
         );
+       
+        // add player ( address, no move, registered, no move)
+        Player memory currenPlayer = Player(msg.sender, Move.Wait, true, false);
         
-        //players[msg.sender] = Player(Move.Wait, true, false);
-        gamers.push(Player(msg.sender, Move.Wait, true, false));
+        players[msg.sender] = currenPlayer;
+        gamers.push(msg.sender);
         
     }
 
@@ -76,29 +79,46 @@ contract RockPaperScissors{
         
     }
 
-    function play() public payable {
-        Move player1Move = gamers[0].move;
-        Move player2Move = gamers[1].move;
+    error OnlyOwner();
+    modifier onlyOwner() {
+        if (msg.sender != gamemaster)
+            revert OnlyOwner();
+        _;
+    }
+
+    function payWinner() public payable onlyOwner{
+        
+    }
+    
+    function play() public onlyOwner{
+        Move player1Move = players[gamers[0]].move;
+        Move player2Move = players[gamers[0]].move;
         
         if(player1Move==player2Move){
-            emit Draw();
+            emit Draw("It is a Draw! Try again , what will the opponent do next time ?");
         }else if(player1Move==Move.Rock){
             if(player2Move==Move.Scissors){
-                emit Win(gamers[0].id,gamers[1].id,0.99 ether,"Rock crushes Scissors");
+                winner = gamers[0];
+                emit Win(gamers[0],gamers[1],0.99 ether,"Rock crushes Scissors");
             }else{
-                emit Win(gamers[1].id,gamers[0].id,0.99 ether,"Paper covers Rock");
+                winner = gamers[1];
+                emit Win(gamers[1],gamers[0],0.99 ether,"Paper covers Rock");
             }
         }else if(player1Move==Move.Scissors){
             if(player2Move==Move.Paper){
-                emit Win(gamers[0].id,gamers[1].id,0.99 ether,"Scissors cuts Paper");
+                winner = gamers[0];
+                emit Win(gamers[0],gamers[1],0.99 ether,"Scissors cuts Paper");
             }else{
-                emit Win(gamers[1].id,gamers[0].id,0.99 ether,"Rock crushes Scissors");
+                winner = gamers[1];
+                emit Win(gamers[1],gamers[0],0.99 ether,"Rock crushes Scissors");
             }
         }else if(player1Move==Move.Paper){
             if(player2Move==Move.Rock){
-                emit Win(gamers[0].id,gamers[1].id,0.99 ether,"Paper covers Rock");
+                winner = gamers[0];
+                emit Win(gamers[0],gamers[1],0.99 ether,"Paper covers Rock");
             }else{
-                emit Win(gamers[1].id,gamers[0].id,0.99 ether,"Scissors cuts Paper");
+                winner = gamers[1];
+                emit Win(gamers[1],gamers[0],0.99 ether,"Scissors cuts Paper");
             }
         }
     }
